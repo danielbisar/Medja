943013aaabc78851169709d5b134c56446e66685
Baruch <danklafft@gmail.com>
2018-02-03 11:05:41 +0100

    SkiaGlLayer

---
+++ Medja.OpenTk/Eval/ILayouter.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Medja.OpenTk.Eval
+{
+    interface ILayouter
+    {
+        Layout GetLayout(PositionInfo positionInfo);
+    }
+}
