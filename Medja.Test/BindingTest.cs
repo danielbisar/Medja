f22530493b4f2b214dff4854c33cec01f219fdfa
Baruch <danklafft@gmail.com>
2018-04-11 16:38:46 +0200

    added missing test file

---
+++ Medja.Test/BindingTest.cs
@@ -0,0 +1,21 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Medja.Test
+{
+    [TestClass]
+    public class BindingTest
+    {
+        //[TestMethod]
+        //public void SimpleBinding()
+        //{
+        //    var source = new MTestObject();
+        //    var target = new MTestObject();
+
+        //    BindingFactory.Create(target._testInt0, source._testInt0);
+
+        //    source.TestInt0 = 10;
+
+        //    Assert.AreEqual(10, target.TestInt0, "Binding did not update the targets value");
+        //}
+    }
+}
