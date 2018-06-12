6db1d9b6b63e9a4b54a46ef29c529ac5d5f81683
Daniel Klafft <danklafft@gmail.com>
2018-06-12 17:55:25 +0200

    added VBO to Medja.OpenTK

---
+++ Medja.OpenTk/Rendering/VertexBufferObject.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using OpenTK;
+using OpenTK.Graphics.OpenGL;
+
+namespace Medja.OpenTk.Rendering
+{
+	public class VertextBufferObject : IDisposable
+	{
+		private int _vertexBufferObjectId;
+		private float[] _data;
+		private bool _isDisposed;
+		private int _dataLengthDiv3;
+
+		public int Id
+		{
+			get { return _vertexBufferObjectId; }
+		}
+
+		public int Length
+		{
+			get { return _data == null ? -1 : _data.Length; }
+		}
+
+		public float[] GetData()
+		{
+			return _data;
+		}
+
+		public VertextBufferObject()
+		{
+			_vertexBufferObjectId = -1;
+		}
+
+		/// <summary>
+		/// Creates the data array without filling it.
+		/// </summary>
+		/// <param name="length">Length.</param>
+		/// <param name="bufferUsageHint">Buffer usage hint.</param>
+		public void CreateData(int length, BufferUsageHint bufferUsageHint = BufferUsageHint.DynamicDraw)
+		{
+			// length of data of VBO can't be changed so we need to recreate it
+			if (_vertexBufferObjectId != -1)
+				GL.DeleteBuffer(_vertexBufferObjectId);
+
+			_data = new float[length];
+			_dataLengthDiv3 = _data.Length / 3;
+
+			_vertexBufferObjectId = GL.GenBuffer();
+			GL.BindBuffer(BufferTarget.ArrayBuffer, _vertexBufferObjectId);
+			GL.BufferData(BufferTarget.ArrayBuffer, _data.Length * sizeof(float), _data, bufferUsageHint);
+		}
+
+		public void UpdateData(List<Vector3> data, BufferUsageHint bufferUsageHint = BufferUsageHint.StaticDraw)
+		{
+			var newDataLength = data.Count * 3;
+
+			if (newDataLength != Length)
+				CreateData(newDataLength, bufferUsageHint);
+
+			UpdateData(ptr =>
+			{
+				unsafe
+				{
+					float* vertexArray = (float*)ptr;
+
+					for (int i = 0; i < data.Count; i++)
+					{
+						vertexArray[i * 3] = data[i].X;
+						vertexArray[i * 3 + 1] = data[i].Y;
+						vertexArray[i * 3 + 2] = data[i].Z;
+					}
+				}
+			});
+		}
+
+		private void UpdateData(Action<IntPtr> update)
+		{
+			GL.EnableClientState(ArrayCap.VertexArray);
+
+			GL.BindBuffer(BufferTarget.ArrayBuffer, _vertexBufferObjectId);
+			GL.VertexPointer(3, VertexPointerType.Float, Vector3.SizeInBytes, 0);
+
+			var verticesPtr = GL.MapBuffer(BufferTarget.ArrayBuffer, BufferAccess.WriteOnly);
+
+			update(verticesPtr);
+
+			GL.UnmapBuffer(BufferTarget.ArrayBuffer);
+			GL.BindBuffer(BufferTarget.ArrayBuffer, 0);
+			GL.DisableClientState(ArrayCap.VertexArray);
+		}
+
+		public void Draw(PrimitiveType type)
+		{
+			GL.EnableClientState(ArrayCap.VertexArray);
+
+			GL.BindBuffer(BufferTarget.ArrayBuffer, _vertexBufferObjectId);
+			GL.VertexPointer(3, VertexPointerType.Float, Vector3.SizeInBytes, 0);
+
+			GL.DrawArrays(type, 0, _dataLengthDiv3);
+
+			GL.BindBuffer(BufferTarget.ArrayBuffer, 0);
+			GL.DisableClientState(ArrayCap.VertexArray);
+		}
+
+		public void UpdateAndDraw(PrimitiveType type, Action<IntPtr> update)
+		{
+			GL.EnableClientState(ArrayCap.VertexArray);
+
+			GL.BindBuffer(BufferTarget.ArrayBuffer, _vertexBufferObjectId);
+			GL.VertexPointer(3, VertexPointerType.Float, Vector3.SizeInBytes, 0);
+
+			var verticesPtr = GL.MapBuffer(BufferTarget.ArrayBuffer, BufferAccess.WriteOnly);
+
+			update(verticesPtr);
+
+			GL.UnmapBuffer(BufferTarget.ArrayBuffer);
+
+			GL.DrawArrays(type, 0, _dataLengthDiv3);
+
+			GL.BindBuffer(BufferTarget.ArrayBuffer, 0);
+			GL.DisableClientState(ArrayCap.VertexArray);
+		}
+
+		public void Dispose()
+		{
+			if (!_isDisposed)
+			{
+				_isDisposed = true;
+				GL.DeleteBuffer(_vertexBufferObjectId);
+			}
+		}
+	}
+}
---
+++ Medja/Linq/ExtendDictionary.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Medja.Linq
+{
+	public static class ExtendDictionary
+	{
+		/// <summary>
+		/// Gets or adds the value for the given key.
+		/// </summary>
+		/// <returns>The value.</returns>
+		/// <param name="dictionary">The Dictionary.</param>
+		/// <param name="key">The key.</param>
+		/// <param name="factory">A method that returns a new object for the 
+		/// given key.</param>
+		/// <typeparam name="TKey">The key type parameter.</typeparam>
+		/// <typeparam name="TValue">The value type parameter.</typeparam>
+		public static TValue GetOrAdd<TKey, TValue>(
+			this IDictionary<TKey, TValue> dictionary,
+			TKey key,
+			Func<TKey, TValue> factory)
+		{
+			if (dictionary.TryGetValue(key, out var value))
+				return value;
+
+			value = factory(key);
+			dictionary.Add(key, value);
+
+			return value;
+		}
+	}
+}
