58f6a23369ee955baee839c4455ca302e951508c
Daniel Klafft <danklafft@gmail.com>
2018-08-07 17:57:01 +0200

    added missing file ControlRenderer.cs

---
+++ Medja.OpenTk/Rendering/ControlRenderer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Medja.Theming;
+using SkiaSharp;
+using Medja.Controls;
+
+namespace Medja.OpenTk.Rendering
+{
+	public class ControlRenderer : ControlRendererBase<SKCanvas, Control>
+	{
+		protected override void Render(SKCanvas canvas, Control control)
+		{
+			var position = control.Position;
+			var skRect = position.ToSKRect();
+
+			using (var paint = new SKPaint())
+			{
+				paint.IsAntialias = true;
+
+				if (control.Background != null)
+				{
+					paint.Color = control.Background.ToSKColor();
+					canvas.DrawRect(skRect, paint);
+				}
+			}
+		}
+	}
+}
