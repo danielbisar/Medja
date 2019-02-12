9f1f4bf5f687697fe5be69d2befb17dfa3e85606
Baruch <danklafft@gmail.com>
2019-02-12 16:36:55 +0100

    ConfirmableDialog: added ok/cancel button, modified classes that inherit from it

--- MedjaOpenGlTestApp/Program.cs
+++ MedjaOpenGlTestApp/Program.cs
@@ -32,7 +32,7 @@ namespace MedjaOpenGlTestApp
 			//var test = new ComboBoxTest(controlFactory);
 			//var test = new ContentControlTest(controlFactory);
 			//var test = new Control3DTest(controlFactory);
-			//var test = new DialogTest(controlFactory);
+			var test = new DialogTest(controlFactory);
 			//var test = new DockPanelTest(controlFactory);
 			//var test = new ImageButtonTest(controlFactory);
 			//var test = new MultithreadingTest(controlFactory);
@@ -44,7 +44,7 @@ namespace MedjaOpenGlTestApp
 			//var test = new TabControlTest(controlFactory);
 			//var test = new TextBoxTest(controlFactory);
 			//var test = new TouchButtonListTest(controlFactory);
-			var test = new VerticalStackPanelTest(controlFactory);
+			//var test = new VerticalStackPanelTest(controlFactory);
 			//var test = new VisibilityTest(controlFactory);
 			
 			_window = application.CreateWindow();
