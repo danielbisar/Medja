8c29ed832e865ffeb2811c7d5574f5be712e983b
Daniel Klafft <danklafft@gmail.com>
2019-04-05 12:01:03 +0200

    Updated version number and copyright notice

--- Medja.OpenTk/Properties/AssemblyInfo.cs
+++ Medja.OpenTk/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("SprintWorx GmbH")]
 [assembly: AssemblyProduct("Medja.OpenTk")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyCopyright("Copyright ©  2018-2019 SprintWORX GmbH")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.2.68.0")]
-[assembly: AssemblyFileVersion("0.2.68.0")]
+[assembly: AssemblyVersion("0.2.7.0")]
+[assembly: AssemblyFileVersion("0.2.7.0")]
