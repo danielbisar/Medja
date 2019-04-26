2ac1c4536ad4cdf1aebe7ce9eb0a6db6272abeb0
Daniel Klafft <danklafft@gmail.com>
2019-04-26 17:13:07 +0200

    added support lib instead lib64 folder - needed for debian

--- src/native/build.sh
+++ src/native/build.sh
@@ -8,5 +8,9 @@ cd out || exit 1
 cmake .. || exit 1
 make || exit 1
 
-cp -u lib64/libglfw.so.3.3 .
-
+# check if glfws output folder is lib or lib64
+if [ -d "lib64" ]; then
+    cp -u lib64/libglfw.so.3.3 .
+else
+    cp -u lib/libglfw.so.3.3 .
+fi
