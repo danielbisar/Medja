9cf571b55ba5a95ee28ad045902b2da36fd8fc2c
Daniel Bişar <daniel.bisar@gmail.com>
2019-05-16 19:13:45 +0200

    ComboBox: implemented drop down click handling

--- src/Medja/Controls/ComboBox/ComboBox2.cs
+++ src/Medja/Controls/ComboBox/ComboBox2.cs
@@ -1,3 +1,6 @@
+using System;
+using Medja.Primitives;
+
 namespace Medja.Controls
 {
     /// <summary>
@@ -16,10 +19,29 @@ namespace Medja.Controls
             get { return PropertyTitle.Get(); }
             set { PropertyTitle.Set(value); }
         }
+        
+        public Property<bool> PropertyIsDropDownOpen;
 
+        public bool IsDropDownOpen
+        {
+            get { return PropertyIsDropDownOpen.Get(); }
+            set { PropertyIsDropDownOpen.Set(value); }
+        }
+        
         public ComboBox2()
         {
             PropertyTitle = new Property<string>();
+            PropertyIsDropDownOpen = new Property<bool>();
+            
+            InputState.Clicked += OnClicked;
+        }
+
+        protected virtual void OnClicked(object sender, EventArgs e)
+        {
+            var mousePos = InputState.PointerPosition;
+
+            if (Position.IsWithin(mousePos))
+                IsDropDownOpen = !IsDropDownOpen;
         }
     }
 }
