8e5806fef24d5a7288b0a28500d891c8df08536b
Daniel Bişar <daniel.bisar@gmail.com>
2019-11-14 16:25:41 +0100

    Add overload for Menu class that allows Command as parameter

--- src/prototypes/Menu/Menu/Menu.cs
+++ src/prototypes/Menu/Menu/Menu.cs
@@ -6,14 +6,14 @@ namespace Menu
     {
         private readonly List<MenuItem> _items;
 
-        public IReadOnlyList<MenuItem> Items
-        {
-            get { return _items; }
-        }
-        
         public Menu()
         {
             _items = new List<MenuItem>();
+        }
+
+        public IReadOnlyList<MenuItem> Items
+        {
+            get { return _items; }
         }
 
         /// <summary>
@@ -25,7 +25,16 @@ namespace Menu
         {
             var result = new MenuItem(title);
             _items.Add(result);
-            
+
+            return result;
+        }
+
+        public MenuItem Add(Command command)
+        {
+            var result = Add(command.Title);
+
+            result.Command = command;
+
             return result;
         }
     }
