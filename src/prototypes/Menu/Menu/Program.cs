e0a59b38c23b800c47f7daa852efb4680ac0e056
Daniel Bişar <daniel.bisar@gmail.com>
2019-11-14 16:06:46 +0100

    print content of Command class in PrintMenu

--- src/prototypes/Menu/Menu/Program.cs
+++ src/prototypes/Menu/Menu/Program.cs
@@ -20,26 +20,73 @@ namespace Menu
             PrintMenu(menu);
         }
 
+        // usage of print methods allows easier exchange of output
+        // without fully abstracting it now
         private static void PrintMenu(Menu menu)
         {
             PrintMenuItems("", menu.Items);
+        }
+
+        private static void Print(string v)
+        {
+            Console.Write(v);
+        }
+
+        private static void PrintSur(string v1, string v2, string v3)
+        {
+            Console.Write(v1 + v2 + v3);
+        }
+
+        // INN = If Not Null
+        private static void PrintINN(string value)
+        {
+            if (value != null)
+                Console.Write(value);
+        }
+
+        private static void PrintSurINN(string v1, string v2)
+        {
+            if (v1 != null && v2 != null)
+                PrintSur(v2, v1, v2);
+        }
+
+        private static void PrintSurINN(string v1, string v2, string v3)
+        {
+            if (v1 != null && v2 != null && v3 != null)
+                PrintSur(v1, v2, v3);
+        }
+
+        private static void PrintLn()
+        {
+            Console.WriteLine();
+        }
+
+        private static void PrintLn(string value)
+        {
+            Console.WriteLine(value);
         }
 
         private static void PrintMenuItems(string indention, IEnumerable<MenuItem> items)
         {
             foreach (var item in items)
             {
-                Console.Write(indention + "{ ");
-                Console.Write(item.Title);
+                Print(indention + "{ ");
+                PrintSur("\"", item.Title, "\"");
+
+                if (item.Command != null)
+                {
+                    PrintSurINN(item.Command.KeyboardShortcut, ", ");
+                    PrintSurINN("\"", item.Command.Description, "\"");
+                }
 
                 if (item.HasChildren)
                 {
-                    Console.WriteLine();
+                    PrintLn();
                     PrintMenuItems(indention + "    ", item.Children);
-                    Console.WriteLine(indention + "}");
+                    PrintLn(indention + "}");
                 }
                 else
-                    Console.WriteLine(" }");
+                    PrintLn(" }");
             }
         }
     }
