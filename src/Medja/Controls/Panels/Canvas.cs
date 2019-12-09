155a06c7c01d6e95a92868240cc17c74942bcc73
Daniel Bişar <daniel.bisar@gmail.com>
2019-12-09 14:36:42 +0100

    panels: add layerpanel

--- src/Medja/Controls/Panels/Canvas.cs
+++ src/Medja/Controls/Panels/Canvas.cs
@@ -3,15 +3,15 @@ using System.Linq;
 
 namespace Medja.Controls
 {
-	/// <summary>
-	/// A Panel that allows its children to be positioned freely.
-	/// Use attached property X and Y available in this class.
-	/// </summary>
-	public class Canvas : Panel
-	{
-		// the attached X and Y is the initial position provided by the control
-		public static readonly int AttachedXId = AttachedPropertyIdFactory.NewId();
-		public static readonly int AttachedYId = AttachedPropertyIdFactory.NewId();
+    /// <summary>
+    /// A Panel that allows its children to be positioned freely.
+    /// Use attached property X and Y available in this class.
+    /// </summary>
+    public class Canvas : Panel
+    {
+        // the attached X and Y is the initial position provided by the control
+        public static readonly int AttachedXId = AttachedPropertyIdFactory.NewId();
+        public static readonly int AttachedYId = AttachedPropertyIdFactory.NewId();
         
         private static bool HasAttachedXY(Control child)
         {
@@ -21,20 +21,20 @@ namespace Medja.Controls
                    && attachedProperties.ContainsKey(AttachedYId);
         }
 
-		public override void Arrange(Size availableSize)
-		{
-			var x = Position.X;
-			var y = Position.Y;
+        public override void Arrange(Size availableSize)
+        {
+            var x = Position.X;
+            var y = Position.Y;
 
-			foreach (var child in Children.Where(HasAttachedXY))
-			{
-				child.Position.X = x + child.GetAttachedProperty<float>(AttachedXId) + child.Margin.Left;
-				child.Position.Y = y + child.GetAttachedProperty<float>(AttachedYId) + child.Margin.Top;
+            foreach (var child in Children.Where(HasAttachedXY))
+            {
+                child.Position.X = x + child.GetAttachedProperty<float>(AttachedXId) + child.Margin.Left;
+                child.Position.Y = y + child.GetAttachedProperty<float>(AttachedYId) + child.Margin.Top;
 
-				child.Arrange(new Size(child.Position.Width, child.Position.Height));
-			}
+                child.Arrange(new Size(child.Position.Width, child.Position.Height));
+            }
 
             base.Arrange(availableSize);
-		}
-	}
+        }
+    }
 }
---
+++ src/Medja/Controls/Panels/LayerPanel.cs
@@ -0,0 +1,51 @@
+using Medja.Primitives;
+
+namespace Medja.Controls
+{
+    /// <summary>
+    /// A panel that resizes it's children to it's own size.
+    /// </summary>
+    public class LayerPanel : Panel
+    {
+        public override void Arrange(Size availableSize)
+        {
+            var x = Position.X + Padding.Left;
+            var y = Position.Y + Padding.Top;
+
+            var height = availableSize.Height - Padding.TopAndBottom;
+            var width = availableSize.Width - Padding.LeftAndRight;
+
+            foreach (var child in Children)
+            {
+                var pos = child.Position;
+                var margin = child.Margin;
+
+                if (child.VerticalAlignment == VerticalAlignment.Top
+                    || child.VerticalAlignment == VerticalAlignment.None
+                    || child.VerticalAlignment == VerticalAlignment.Stretch)
+                    pos.Y = y + margin.Top;
+                else if (child.VerticalAlignment == VerticalAlignment.Bottom)
+                    pos.Y = y + height - (pos.Height + margin.Bottom);
+                
+                if(child.VerticalAlignment == VerticalAlignment.None
+                   || child.VerticalAlignment == VerticalAlignment.Stretch)
+                    pos.Height = height - margin.TopAndBottom;
+
+                if(child.HorizontalAlignment == HorizontalAlignment.Left
+                   || child.HorizontalAlignment == HorizontalAlignment.None
+                   || child.HorizontalAlignment == HorizontalAlignment.Stretch)
+                    pos.X = x + margin.Left;
+                else if(child.HorizontalAlignment == HorizontalAlignment.Right)
+                    pos.X = x + width - (pos.Width + margin.Right);
+                
+                if(child.HorizontalAlignment == HorizontalAlignment.None
+                   || child.HorizontalAlignment == HorizontalAlignment.Stretch)
+                    pos.Width = width - margin.LeftAndRight;
+
+                child.Arrange(new Size(pos.Width, pos.Height));
+            }
+
+            base.Arrange(availableSize);
+        }
+    }
+}
