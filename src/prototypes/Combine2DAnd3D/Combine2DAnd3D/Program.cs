c7cc3e7e8667f6c498edfcc5b48ff0734e8ce291
Daniel Bişar <daniel.bisar@gmail.com>
2020-01-13 17:02:54 +0100

    example for combining 2d and 3d rendering

--- src/prototypes/Combine2DAnd3D/Combine2DAnd3D/Program.cs
+++ src/prototypes/Combine2DAnd3D/Combine2DAnd3D/Program.cs
@@ -2,9 +2,12 @@
 using Medja;
 using Medja.Controls;
 using Medja.OpenTk;
+using Medja.OpenTk.Components3D;
 using Medja.OpenTk.Controls;
 using Medja.OpenTk.Themes.DarkBlue;
+using Medja.Primitives;
 using Medja.Theming;
+using OpenTK;
 using OpenTK.Graphics.OpenGL4;
 
 namespace Combine2DAnd3D
@@ -39,20 +42,46 @@ namespace Combine2DAnd3D
             window.CenterOnScreen(800, 600);
 
             var openGLEventControl = _controlFactory.Create<OpenGLEventControl>();
+            openGLEventControl.Load += On3DControlLoad;
             openGLEventControl.RenderControl += OnRender3DControl;
 
+            var stackPanel = _controlFactory.Create<VerticalStackPanel>();
+            stackPanel.Position.Width = 200;
+            stackPanel.HorizontalAlignment = HorizontalAlignment.Left;
+            stackPanel.Add(_controlFactory.CreateTextBlock("Test1"));
+            stackPanel.Add(_controlFactory.CreateTextBlock("Test2"));
+            stackPanel.IsTopMost = true;
+            
             var layerPanel = _controlFactory.Create<LayerPanel>();
             layerPanel.Add(openGLEventControl);
+            layerPanel.Add(stackPanel);
             
             window.Content = layerPanel;
 
             return window;
         }
 
+        private GLScene _scene;
+
+        private void On3DControlLoad(object sender, EventArgs e)
+        {
+            _scene = new GLScene();
+            _scene.Camera = new GLOrthographicCamera
+            {
+                Width = 10,
+                Height = 10,
+                Position = new Vector3(0, 0, 5)
+            };
+            _scene.Add(new GLCuboid());
+        }
+
         private void OnRender3DControl(object sender, EventArgs e)
         {
             GL.ClearColor(0, 0, 0, 1);
             GL.Clear(ClearBufferMask.ColorBufferBit);
+
+            _scene.Render();
+            
             GL.Flush();
         }
     }
